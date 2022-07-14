2367b8a73f525bc66a77ea7c270148d4cbb669a6
Olivier HAMON <olivier.hmn@gmail.com>
2022-07-14 21:46:07 +0200

    fix: remove vue

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from "astro/config";
 
-import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-import turbolinks from "@astrojs/turbolinks";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
@@ -11,5 +8,5 @@ export default defineConfig({
   server: {
     port: 3030,
   },
-  integrations: [vue(), tailwind(), turbolinks(), partytown(), sitemap()],
+  integrations: [tailwind(), sitemap()],
 });
