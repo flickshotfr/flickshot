64af3cdc42b6d69906c365421fa5cc698ce7fc19
Olivier HAMON <olivier.hmn@gmail.com>
2022-07-15 01:26:42 +0200

    fix: astro build

---
+++ app/src/pages/articles/_types.ts
@@ -0,0 +1,14 @@
+export type Article = {
+    title: string
+    slug: string
+    description: string
+    type: string
+    language: string
+    date: string
+    lastmod: string
+    views: number
+    author: string
+    country: string
+    categories: string[]
+    tags: string[]
+};
